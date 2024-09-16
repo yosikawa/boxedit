2636871964f94d3c3a5074ffa313a1972ed2f19c
Akihiro Yoshikawa <yosikawa@altalk.com>
2024-09-16 13:05:21 +0900

    init

---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "boxedit" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('boxedit.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from boxedit!');
+	});
+
+	context.subscriptions.push(disposable);
+
+	const disposable2 = vscode.commands.registerCommand('boxedit.reverseWord', function() {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const word = document.getText(selection);
+			const reversed = word.split('').reverse().join('');
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, reversed);
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable2);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
